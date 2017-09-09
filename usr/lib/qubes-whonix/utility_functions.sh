01964e3c8c53b49aa14e56f7924fce5e88b5a448
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-09-09 14:25:00 +0200

    Use appropriate updates proxy if it is qrexec based
    In Qubes 4.0, updates proxy runs on qrexec and templates have no other
    network access. This is accompanied with local TCP listener redirecting
    the traffic to the proxy.
    If the listener is active, use its address.
    This variable is (currently) used only to check if the proxy is
    torified. Actual proxy setting in apt is done by standard Qubes startup
    scripts.

--- usr/lib/qubes-whonix/utility_functions.sh
+++ usr/lib/qubes-whonix/utility_functions.sh
@@ -23,7 +23,11 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # 'apt-get' proxy server
-PROXY_SERVER='http://10.137.255.254:8082/'
+if systemctl -q is-active qubes-updates-proxy-forwarder.socket; then
+    PROXY_SERVER='http://127.0.0.1:8082/'
+else
+    PROXY_SERVER='http://10.137.255.254:8082/'
+fi
 
 # A magic line in the error pages tinyproxy (from qubes-updates-proxy) and
 # squid (qubes-updates-cache) send on Whonix when something connects to their
