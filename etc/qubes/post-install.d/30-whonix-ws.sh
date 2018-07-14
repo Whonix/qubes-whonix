436a278cf1e9f92e046b1ecf8a8dd97b9364a039
Patrick Schleizer <adrelanos@riseup.net>
2018-07-14 17:46:07 +0800

    qvm-features-request whonix-gw=1

--- etc/qubes/post-install.d/30-whonix-ws.sh
+++ etc/qubes/post-install.d/30-whonix-ws.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -f "/usr/share/anon-gw-base-files/gateway" ]; then
+   qvm-features-request whonix-gw=1
+fi
+
 if [ -f "/usr/share/anon-ws-base-files/workstation" ]; then
    qvm-features-request whonix-ws=1
 fi
+
+true
