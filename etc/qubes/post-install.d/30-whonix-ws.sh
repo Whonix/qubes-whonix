b301ff22e4523630c6d62eb81c2758360e7ee680
Aaron Rainbolt <arraybolt3@gmail.com>
2026-08-14 16:40:03 -0400

    Fixes, FIXMEs, TODOs

--- etc/qubes/post-install.d/30-whonix-ws.sh
+++ etc/qubes/post-install.d/30-whonix-ws.sh
@@ -5,6 +5,12 @@
 
 ## Better not setting errtrace ('set -e').
 ## https://github.com/QubesOS/qubes-issues/issues/4080#issuecomment-405100531
+##
+## FIXME: Either get rid of strict mode here, or get rid of the above comment
+## since we now set errexit and errtrace.
+##
+## FIXME: This file is poorly named, it is useful on both the gateway and
+## workstation and contains code for both.
 
 set -o errexit
 set -o nounset
@@ -20,7 +26,3 @@ fi
 if [ -f "/usr/share/anon-ws-base-files/workstation" ]; then
    qvm-features-request whonix-ws=1 || true
 fi
-
-## Exit success even in case above command failed to avoid breaking the apt-get
-## package management system.
-true
